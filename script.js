8f35a9c849702b71697cfcb09b585fcb9cdea7bd
muhfadhil <mfadhil@stundents.ac.id>
2021-12-19 04:39:01 +0700

    mengubah layout jumbotron dan menghapus script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// my name
-const myName = document.querySelector(".my-name");
-const myNameSpread = [...myName.textContent];
-
-// myNameSpread.map((s) => `<span>${s}</span>`);
-const mySpan = myNameSpread.map((s) => `<span>${s}</span>`).join("");
-
-myName.innerHTML = mySpan;
